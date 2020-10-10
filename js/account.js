8d85ea6b1ed702a064d0375bb32592f0c4a895e4
Dejan Antic <dex.antic@gmail.com>
2020-10-10 23:23:18 +0200

    Add jQuery validation

--- js/account.js
+++ js/account.js
@@ -1,21 +1,52 @@
 $(function () {
   loaderMethods.remove();
 
-  function checkPasswords() {
-    const passOne = $('#inputPassword');
-    const passTwo = $('#inputPassword2');
+  $.validator.setDefaults({
+    errorElement: 'div',
+    errorClass: 'is-invalid',
+    validClass: 'is-valid',
+    errorPlacement(error, element) {
+      error.addClass('invalid-feedback');
+      element.closest('.form-group').append(error);
+    },
+    highlight(element, errorClass, validClass) {
+      $(element).addClass(errorClass);
+    },
+    unhiglight(element, errorClass, validClass) {
+      $(element).removeClass(errorClass);
+    },
+  });
 
-    if (passOne.val() !== passTwo.val()) {
-      passOne.val('');
-      passTwo.val('');
-    }
-  }
-
-  $('#account-form').on('submit', function (e) {
-    e.preventDefault();
-
-    checkPasswords();
-
-    $(this).addClass('was-validated');
+  $('#account-form').validate({
+    rules: {
+      fname: 'required',
+      lname: 'required',
+      email: {
+        required: true,
+        email: true,
+      },
+      password1: {
+        required: true,
+        minlength: 6,
+      },
+      password2: {
+        required: true,
+        minlength: 6,
+        equalTo: '#inputPassword',
+      },
+      city: 'required',
+      country: 'required',
+    },
+    messages: {
+      fname: 'Please provide a name',
+      lname: 'Please provide a last name',
+      email: 'Please provide a valid email',
+      city: 'Please provide a city',
+      country: 'Please provide a country',
+    },
+    submitHandler() {
+      $('#account-form').trigger('reset');
+      $('input').removeClass(['is-valid', 'is-invalid']);
+    },
   });
 });
